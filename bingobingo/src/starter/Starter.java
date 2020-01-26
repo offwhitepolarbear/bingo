df067b04ecff9bb3043b77774495509eb35ddf3d
kohn <kohn@DESKTOP-VJI0H4M>
2020-01-26 14:57:05 +0900

    flexible player

--- bingobingo/src/starter/Starter.java
+++ bingobingo/src/starter/Starter.java
@@ -8,19 +8,18 @@ import checker.GameClearJudge;
 import numberPicker.NumberPicker;
 import player.BingoDirection;
 import player.Player;
+import player.PlayerListMaker;
 import player.PlayerMaker;
 
 public class Starter {
 	static int bingoBoardSize = 5;
 	static int maxIntRange = 100;
-	static int bingoLinesForClear = 1;
+	static int bingoLinesForClear = 2;
+	static int howManyPlayers = 4;
 	
 	public static void main(String[] args) {
 		ArrayList<Integer> numberIndexing = NumberPicker.numberSequenceMaker(maxIntRange, maxIntRange);
-		ArrayList<Player> playerList = new ArrayList<Player>();
-		playerList.add(PlayerMaker.playerMaker(maxIntRange, bingoBoardSize, "1번 유저"));
-		playerList.add(PlayerMaker.playerMaker(maxIntRange, bingoBoardSize, "2번 유저"));
-		playerList.add(PlayerMaker.playerMaker(maxIntRange, bingoBoardSize, "4번 유저"));
+		ArrayList<Player> playerList = PlayerListMaker.playerListMaker(maxIntRange, bingoBoardSize, howManyPlayers);
 		
 		while(!GameClearJudge.gameCleareJudge(playerList, bingoLinesForClear)) {
 			int pickNumberNow = numberIndexing.get(0);
